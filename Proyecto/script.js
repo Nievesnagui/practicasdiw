c54bd80299e396b1b45a68d5736e52dc0741e5f4
Nieves <smilenvs@gmail.com>
2023-11-20 13:27:46 +0100

    Proyecto terminado

--- Proyecto/script.js
+++ Proyecto/script.js
@@ -1,21 +1,53 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Get all "navbar-burger" elements
     const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-  
-    // Add a click event on each of them
+
+    // Agrega el evento click al enlace con id informacion-link
+    const informacionLink = document.getElementById('informacion-link');
+    if (informacionLink) {
+        informacionLink.addEventListener('click', () => {
+            showContent('informacion');
+        });
+    }
+
+    // Funcionalidad de tabs
     $navbarBurgers.forEach(el => {
         el.addEventListener('click', () => {
-            // Get the target from the "data-target" attribute
             const target = el.dataset.target;
             const $target = document.getElementById(target);
-  
-            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
             el.classList.toggle('is-active');
             $target.classList.toggle('is-active');
         });
     });
+    const tabSystem = {
+        init(){
+            document.querySelectorAll('.tabs-menu').forEach(tabMenu => {
+                Array.from(tabMenu.children).forEach((child, ind) => {
+                    child.addEventListener('click', () => {
+                        tabSystem.toggle(child.dataset.target);
+                    });
+                    if(child.className.includes('is-active')){
+                        tabSystem.toggle(child.dataset.target);
+                    }
+                });
+            });
+        },
+        toggle(targetId){
+            document.querySelectorAll('.tab-content').forEach(contentElement=>{
+                contentElement.style.display = contentElement.id === targetId ? 'block' : 'none';
+                document.querySelector(`[data-target="${contentElement.id}"]`).classList[contentElement.id === targetId ? 'add' : 'remove']('is-active');
+            })
+        },
+    };
+    tabSystem.init()
+
+    (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+        const $notification = $delete.parentNode;
     
+        $delete.addEventListener('click', () => {
+          $notification.parentNode.removeChild($notification);
+        });
+      });
   });
-  
 
   
